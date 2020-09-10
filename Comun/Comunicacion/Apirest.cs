9ea2ca736c62aa59c7447dc4181412040e1f5e26
Carlos <Carlos@DESKTOP-EFJGSS3>
2020-09-10 15:19:37 -0500

    se agrego paquetes NuGet para JSON

---
+++ Comun/Comunicacion/Apirest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Windows.Forms;
+
+namespace Comun.Comunicacion
+{
+    public class Apirest
+    {
+        //POR METODO POST
+        protected dynamic Post(string url, string json, string autorizacion)
+        {
+            try
+            {
+                var client = new RestClient(url);
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("content-type", "application/json");
+                request.AddParameter("application/json", json, ParameterType.RequestBody);
+
+                if (autorizacion != null)
+                {
+                    request.AddHeader("Authorization", autorizacion);
+                }
+
+                IRestResponse response = client.Execute(request);
+
+                dynamic datos = JsonConvert.DeserializeObject(response.Content);
+
+                return datos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }       
+
+        //POR METODO GET
+
+    }
+}
---
+++ Comun/Comunicacion/Dapi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun.Comunicacion
+{
+    public class Dapi
+    {
+        public string dni { get; set; }//tiene que ser igual al Json que Enviamos que es igual al que nos trae la consulta al api. por post        
+    }
+
+    public static class RecuperarData
+    {
+        public static string nombres { get; set; }
+        public static string paterno { get; set; }
+        public static string materno { get; set; }
+        public static string mensaje { get; set; }
+    }
+}
