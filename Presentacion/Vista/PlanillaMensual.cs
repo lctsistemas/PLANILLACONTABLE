d76927aaf83b4b8b5c1ca4a15aabb881fb3b373e
iodev <iodev@outlook.es>
2020-09-25 16:06:04 -0500

    Planilla diseño actualizado

---
+++ Presentacion/Vista/PlanillaMensual.cs
@@ -0,0 +1,48 @@
+﻿using Presentacion.Helps;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion.Vista
+{
+    public partial class PlanillaMensual : Form
+    {
+        public PlanillaMensual()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_MouseDown(object sender, MouseEventArgs e)
+        {
+            WindowsMove.ReleaseCapture();
+            WindowsMove.SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void panel6_MouseDown(object sender, MouseEventArgs e)
+        {
+            WindowsMove.ReleaseCapture();
+            WindowsMove.SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void PlanillaMensual_Load(object sender, EventArgs e)
+        {
+            this.Top = (Screen.PrimaryScreen.WorkingArea.Height - this.Height) / 2;
+            this.Left = (Screen.PrimaryScreen.WorkingArea.Width - this.Width) / 2;
+            txtDNI.Focus();
+        }
+    }
+}
