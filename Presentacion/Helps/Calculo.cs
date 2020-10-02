9121ca7bb03446d5ddff5d041efc9d873fb8c050
Carlos <Carlos@DESKTOP-EFJGSS3>
2020-10-02 18:05:20 -0500

    clase calculo

---
+++ Presentacion/Helps/Calculo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion.Helps
+{
+    public class Calculo
+    {
+
+        public static double HoraExDiurna(double basico, double asig_fami, double taza, int hora, int minuto)
+        {
+            double hora_diario =(((basico + asig_fami) / 30 )/ 8);
+            double hora_minuto = (hora_diario / 60);
+            double taza_mashoradiario = (hora_diario * taza) + hora_diario;
+            double taza_mashoraminuto = (hora_minuto * taza) + hora_minuto;
+            double montohora = (taza_mashoradiario * hora);
+            double montominuto = (taza_mashoraminuto * minuto);
+            return (montohora + montominuto);
+        }
+      
+    }
+}
