40b3a8147f241705e0532bd436d613e8974e0df0
Carlos <Carlos@DESKTOP-EFJGSS3>
2020-11-20 13:47:17 -0500

    clases para concepto de planilla

---
+++ Datos/Repositories/RConceptos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using Datos.Entities;
+
+
+namespace Datos.Repositories
+{
+    public class RConceptos
+    {
+        int result;
+        public int SaveAddUpdate(DConceptos entiti)
+        {
+            result = 0;
+            using (SqlConnection conect = RConexion.Getconectar())
+            {
+                conect.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conect;
+                    cmd.CommandText = "SP_RegistroConceptos";
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@id_conceptos", entiti.Id_conceptos);
+                    cmd.Parameters.AddWithValue("@id_mes", entiti.Id_mes);
+                    cmd.Parameters.AddWithValue("@id_planilla", entiti.Id_planilla);
+                    cmd.Parameters.AddWithValue("@hextraDiurna", entiti.HextraDiurna);
+                    cmd.Parameters.AddWithValue("@hextraNocturna", entiti.HextraNocturna);
+                    cmd.Parameters.AddWithValue("@feriadoDomi", entiti.FeriadoDomi);
+                    cmd.Parameters.AddWithValue("@boniNocturna", entiti.BoniNocturna);
+                    cmd.Parameters.AddWithValue("@primeromayo", entiti.PrimeroMayo);
+                    cmd.Parameters.AddWithValue("@tarda", entiti.Tarda);
+                    cmd.Parameters.AddWithValue("@subsi", entiti.Subsi);
+                    cmd.Parameters.AddWithValue("@thoraex", entiti.Thoraex);
+                    cmd.Parameters.AddWithValue("@otroreinte", entiti.Otroreinte);
+                    cmd.Parameters.AddWithValue("@prest_aliment", entiti.Prest_aliment);
+                    cmd.Parameters.AddWithValue("@gratif", entiti.Gratif);
+                    cmd.Parameters.AddWithValue("@vaca", entiti.Vaca);
+                    cmd.Parameters.AddWithValue("@truncas", entiti.Truncas);
+                    cmd.Parameters.AddWithValue("@reinte_gratiboni", entiti.Reinte_gratiboni);
+                    cmd.Parameters.AddWithValue("@essa_vida", entiti.Essa_vida);
+                    cmd.Parameters.AddWithValue("@adela", entiti.Adela);
+                    cmd.Parameters.AddWithValue("@presta", entiti.Presta);
+                    cmd.Parameters.AddWithValue("@rentquinta", entiti.Rentquinta);
+                    cmd.Parameters.AddWithValue("@reten_judici", entiti.Reten_judici);
+                    cmd.Parameters.AddWithValue("@otrodescu", entiti.Otrodescu);
+                    cmd.Parameters.AddWithValue("@recarg_consu", entiti.Recarg_consu);
+                    result = cmd.ExecuteNonQuery();
+
+                    cmd.Parameters.Clear();
+                }
+            }
+            return result;
+        }
+
+        public DataTable GetData(DConceptos entiti)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Negocio/Models/NConceptos.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Negocio.ValueObjects;
+using Datos.Repositories;
+using Datos.Entities;
+
+namespace Negocio.Models
+{
+    public class NConceptos:IDisposable
+    {
+        public int Id_conceptos { get; set; }
+        public int Id_mes { get; set; }
+        public int Id_planilla { get; set; }
+        public bool HextraDiurna { get; set; }
+        public bool HextraNocturna { get; set; }
+        public bool FeriadoDomi { get; set; }
+        public bool BoniNocturna { get; set; }
+        public bool PrimeroMayo { get; set; }
+        public bool Tarda { get; set; }
+        public bool Subsi { get; set; }
+        public bool Thoraex { get; set; }
+        public bool Otroreinte { get; set; }
+        public bool Prest_aliment { get; set; }
+        public bool Gratif { get; set; }
+        public bool Vaca { get; set; }
+        public bool Truncas { get; set; }
+        public bool Reinte_gratiboni { get; set; }
+        public bool Essa_vida { get; set; }
+        public bool Adela { get; set; }
+        public bool Presta { get; set; }
+        public bool Rentquinta { get; set; }
+        public bool Reten_judici { get; set; }
+        public bool Otrodescu { get; set; }
+        public bool Recarg_consu { get; set; }
+
+        public EntityState State { get; set; }
+        private readonly RConceptos rconcep;
+
+        public NConceptos()
+        {
+            rconcep = new RConceptos();
+        }
+
+        //METODO PARA ADD, UPDATE
+        public string SaveChanges()
+        {
+           
+            string message;
+            using (DConceptos dcon = new DConceptos())
+            {
+                try
+                {
+
+                    dcon.Id_conceptos = Id_conceptos;
+                    dcon.Id_mes = Id_mes;
+                    dcon.Id_planilla = Id_planilla;
+                    dcon.HextraDiurna = HextraDiurna;
+                    dcon.HextraNocturna = HextraNocturna;
+                    dcon.FeriadoDomi = FeriadoDomi;
+                    dcon.BoniNocturna = BoniNocturna;
+                    dcon.PrimeroMayo = PrimeroMayo;
+                    dcon.Tarda = Tarda;
+                    dcon.Subsi = Subsi;
+                    dcon.Thoraex = Thoraex;
+                    dcon.Otroreinte = Otroreinte;
+                    dcon.Prest_aliment = Prest_aliment;
+                    dcon.Gratif = Gratif;
+                    dcon.Vaca = Vaca;
+                    dcon.Truncas = Truncas;
+                    dcon.Reinte_gratiboni = Reinte_gratiboni;
+                    dcon.Essa_vida = Essa_vida;
+                    dcon.Adela = Adela;
+                    dcon.Presta = Presta;
+                    dcon.Rentquinta = Rentquinta;
+                    dcon.Reten_judici = Reten_judici;
+                    dcon.Otrodescu = Otrodescu;
+                    dcon.Recarg_consu = Recarg_consu;
+
+                    switch (State)
+                    {
+                        case EntityState.Guardar:
+                            rconcep.SaveAddUpdate(dcon);
+                            message = "¡Guardado!";
+                            break;
+
+                        default:
+                            message = "Error in Transaction!";
+                            break;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    message = ex.Message;
+                }
+                finally { }                
+            }
+            return message;
+        }
+
+        
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
