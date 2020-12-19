b860bcebd18a6d396af2290528bfccae6c4a30f2
Carlos <Carlos@DESKTOP-EFJGSS3>
2020-12-18 21:01:12 -0500

    se agrego imagenes

---
+++ Presentacion/Subvista/Vista_rol.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Presentacion.Subvista
+{
+    partial class FrmVista_rol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_rol = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel_rol
+            // 
+            this.panel_rol.BackColor = System.Drawing.Color.White;
+            this.panel_rol.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_rol.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_rol.Location = new System.Drawing.Point(0, 0);
+            this.panel_rol.Name = "panel_rol";
+            this.panel_rol.Size = new System.Drawing.Size(470, 260);
+            this.panel_rol.TabIndex = 0;
+            // 
+            // FrmVista_rol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 260);
+            this.Controls.Add(this.panel_rol);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmVista_rol";
+            this.Text = "Vista_rol";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_rol;
+    }
+}
